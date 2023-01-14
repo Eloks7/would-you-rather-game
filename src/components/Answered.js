c0135001af62dc1ac18380fc47ba8a6ec87fdfaf
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-14 13:15:40 +0100

    Create Answered.js

---
+++ src/components/Answered.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Answered extends Component {
+    render() {
+        return (
+            <div>
+                LearderBoard
+            </div>
+        )
+    }
+}
+
+export default Answered;
