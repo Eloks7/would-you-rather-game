d5f8a133209460e43dd943a1a7a5507d4c565a72
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-15 21:51:47 +0100

    Update NewQuestion.js

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,13 +1,119 @@
 import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { handleAddQuestion } from '../actions/shared';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class NewQuestion extends Component {
-    render() {
+    state = {
+        optionOneText: '',
+        optionTwoText: '',
+        toHome: false,
+      }
+      handleChangeOne = (e) => {
+        const optionOneText = e.target.value
+    
+        this.setState(() => ({
+          optionOneText,
+        }))
+      }
+      handleChangeTwo = (e) => {
+        const optionTwoText = e.target.value
+    
+        this.setState(() => ({
+          optionTwoText,
+        }))
+      }
+      handleSubmit = (e) => {
+        e.preventDefault()
+    
+        const { optionOneText, optionTwoText } = this.state
+        const { dispatch, authedUser } = this.props;
+        const author = authedUser;
+  
+        dispatch(handleAddQuestion({optionOneText, optionTwoText, author}));
+    
+        this.setState(() => ({
+          optionOneText: '',
+          optionTwoText: '',
+          toHome: true,
+        }))
+        // return <Redirect to='/' />
+      }
+      render() {
+        const { optionOneText, optionTwoText, toHome } = this.state;
+        
+        if (toHome === true) {
+            return <Redirect to='/' />
+        }
+  
         return (
-            <div>
-                NewQuestion
-            </div>
+          <div>
+            <h3 style={{ marginLeft: '16px'}}>New Question</h3>
+            {/* <form className='new-tweet' onSubmit={this.handleSubmit}>
+              <label>Option 1</label>
+              <textarea
+                placeholder="Enter option one"
+                value={optionOneText}
+                onChange={this.handleChangeOne}
+                className='textarea'
+                maxLength={150}
+              /><br/>
+              <textarea
+                placeholder="enter Option two"
+                value={optionTwoText}
+                onChange={this.handleChangeTwo}
+                className='textarea'
+                maxLength={150}
+              />
+              <button
+                className='btn'
+                type='submit'
+                disabled={optionOneText === '' || optionTwoText === ''}>
+                  Submit
+              </button>
+            </form> */}
+            <Card className="m-3" style={{ width: '40rem', marginBottom: '16px', textAlign: 'left'}}>
+							<Card.Body>
+								<Form onSubmit={this.handleSubmit}>
+									<Form.Group controlId="optionOne">
+										<Form.Label>Option 1</Form.Label>
+										<Form.Control
+											type="text"
+											value={optionOneText}
+											onChange={this.handleChangeOne}
+										/>
+									</Form.Group>
+									<p style={{ marginTop: '16px'}}> OR </p>
+									<Form.Group controlId="optionTwo">
+										<Form.Label>Option 2</Form.Label>
+										<Form.Control
+											type="text"
+											value={optionTwoText}
+											onChange={this.handleChangeTwo}
+										/>
+									</Form.Group>
+									<Button
+                                        style={{marginTop: '32px'}}
+										type="submit"
+										variant="primary"
+										disabled={optionOneText === '' || optionTwoText === ''}
+                                    >
+										Create Question
+									</Button>
+								</Form>
+							</Card.Body>
+						</Card>
+          </div>
         )
     }
 }
 
-export default NewQuestion;
+function mapStateToProps ({ authedUser}) {
+    return {
+        authedUser
+    }
+}
+export default connect(mapStateToProps)(NewQuestion);
