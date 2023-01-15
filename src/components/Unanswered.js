4e33fd98f6f8566274f652015b73cf717294b373
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-15 21:51:57 +0100

    Update Unanswered.js

--- src/components/Unanswered.js
+++ src/components/Unanswered.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import QuestionCard from './QuestionCard';
 
 class Unanswered extends Component {
     render() {
+        const { unansweredIds } = this.props;
+        console.log(unansweredIds)
         return (
-            <div>
-                Unanswered
+            <div className="questions   ">
+                {unansweredIds.map((id) => (
+                    <div style={{ display: 'inline-block', marginRight: '32px' }} key={id}><QuestionCard id={id}/></div>
+                ))}
             </div>
         )
     }
