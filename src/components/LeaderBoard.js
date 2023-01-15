b227ab618eea9455e792bbf706b7c7d263518abc
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-15 21:50:05 +0100

    Update LeaderBoard.js

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import User from './User';
 
 class LeaderBoard extends Component {
     render() {
         return (
             <div>
-                LearderBoard
+                <Card style={{ width: '32rem'}}>
+                    {this.props.orderedUserIds.map((userId) => (
+                        <User key={userId} id={userId} />
+                    ))}
+                </Card>
             </div>
         )
     }
 }
 
-export default LeaderBoard;
+function mapStateToProps({ users }) {
+    const userIds = Object.keys(users);
+    const orderedUserIds = userIds.sort((a, b) => (
+        ((users[b].questions.length) + (Object.keys(users[b].answers).length))
+        -
+        ((users[a].questions.length) + (Object.keys(users[a].answers).length))
+    ))
+
+    return {
+        orderedUserIds
+    }
+
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
