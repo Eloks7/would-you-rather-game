0439a090eb3ab9e258865feaac646b96d658000e
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-15 21:49:52 +0100

    Update Answered.js

--- src/components/Answered.js
+++ src/components/Answered.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import QuestionCard from './QuestionCard';
+import { connect } from 'react-redux';
+
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
 
 class Answered extends Component {
     render() {
+        const { answeredIds } = this.props;
+        console.log(answeredIds)
         return (
-            <div>
-                LearderBoard
+            <div className="questions" >
+                {answeredIds.map((id) => (
+                    <div style={{ display: 'inline-block', marginRight: '32px' }} key={id}><QuestionCard id={id}/></div>
+                ))}
             </div>
         )
     }
 }
 
-export default Answered;
+// function mapStateToProps ({ authedUser, users, questions }) {
+//     console.log(authedUser)
+//     const answered = authedUser !== null
+//                         ? users[authedUser].answers
+//                         : null;
+//     console.log(answered)
+
+//     return {
+//         questionsIds: answered === null
+//         ? undefined
+//         : Object.keys(answered)
+//             .sort((a, b) => answered[b].timestamp - answered[a].timestamp)
+//     }
+// }
+
+export default connect()(Answered);
