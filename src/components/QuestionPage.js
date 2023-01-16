dcca4d4825ba0019adf4a8b59fbc49c954f6f2c9
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-16 22:27:20 +0100

    Update QuestionPage.js

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import QuestionAnswered from './QuestionAnswered';
+import QuestionUnanswered from './QuestionUnanswered';
 
 class QuestionsPage extends Component {
     render() {
+        const { answered, id } = this.props;
         return (
             <div>
-                QuestionPage
+                {/* <h3>Would you rather ...</h3> */}
+                {answered
+                    ? <QuestionAnswered id={id}/>
+                    : <QuestionUnanswered id={id}/>
+                }
             </div>
         )
     }
 }
 
-export default QuestionsPage;
+function mapStateToProps({ users, questions, authedUser }, { id }) {
+    
+    // const question = questions.id;
+    const userAnswers = users[authedUser].answers;
+
+    const answered = userAnswers.hasOwnProperty(id)
+                        ? true
+                        : false;
+    return {
+        answered
+    }
+}
+
+export default connect(mapStateToProps)(QuestionsPage);
