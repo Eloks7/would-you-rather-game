f839b65930b06edae1d3b2c5b3fcfbb9d2e8beb3
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-17 13:12:09 +0100

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -12,7 +12,7 @@ class User extends Component {
                 <Card style={{ width: '24rem', margin: '24px 0', display: 'inline-block'}}>
                     {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                     <Card.Body>
-                        <Card.Title style={{ fontWeight: 700, marginBottom: '32px' }}>{authorName}</Card.Title>
+                        <Card.Title style={{ fontWeight: 700, marginBottom: '20px' }}>{authorName}</Card.Title>
                         {/* <Card.Subtitle className="mb-2 text-muted" style={{fontSize: '14px'}}>author {authorName}</Card.Subtitle> */}
                         <Card.Text style={{ fontWeight: 400, textDecoration: 'none' }}>
                             Questions Answered: {answeredLength}
