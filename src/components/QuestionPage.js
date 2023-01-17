2ee49de15173863008afd6861318de53e0f63ff4
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-17 13:11:56 +0100

    404 page implementation

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -5,13 +5,19 @@ import QuestionUnanswered from './QuestionUnanswered';
 
 class QuestionsPage extends Component {
     render() {
-        const { answered, id } = this.props;
+        const { answered, id, doesNotExist } = this.props;
         return (
             <div>
                 {/* <h3>Would you rather ...</h3> */}
-                {answered
-                    ? <QuestionAnswered id={id}/>
-                    : <QuestionUnanswered id={id}/>
+                {!doesNotExist
+                    ? <div>
+                        <h2>Error 404</h2>
+                        <p>The question you are looking for does not exist</p>
+                    </div>
+                    : answered
+                        ? <QuestionAnswered id={id}/>
+                        : <QuestionUnanswered id={id}/>
+                    
                 }
             </div>
         )
@@ -26,8 +32,13 @@ function mapStateToProps({ users, questions, authedUser }, { id }) {
     const answered = userAnswers.hasOwnProperty(id)
                         ? true
                         : false;
+
+    const doesNotExist = questions.hasOwnProperty(id)
+                            ? true
+                            : false;
     return {
-        answered
+        answered,
+        doesNotExist,
     }
 }
 
