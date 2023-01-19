5e2c94b91d7001cd16e8baad40af08237c27b7f0
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-19 10:08:55 +0100

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
 
 class User extends Component {
     render() {
@@ -24,10 +22,8 @@ class User extends Component {
         return (
             <div className="user" style={{ display: 'inline', margin: 'auto' }}>
                 <Card style={{ width: '24rem', margin: '24px 0', display: 'inline-block'}}>
-                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                     <Card.Body>
                         <Card.Title style={{ fontWeight: 700, marginBottom: '20px' }}>{authorName} ({position})</Card.Title>
-                        {/* <Card.Subtitle className="mb-2 text-muted" style={{fontSize: '14px'}}>author {authorName}</Card.Subtitle> */}
                         <Card.Text style={{ fontWeight: 400, textDecoration: 'none' }}>
                             Questions Answered: {answeredLength}
                         </Card.Text>
@@ -37,7 +33,6 @@ class User extends Component {
                         <Card.Text style={{ fontWeight: 500, textDecoration: 'none' }}>
                             <strong>Total Score: {totalScore}</strong>
                         </Card.Text>
-                        {/* <Link to={`/questions/${id}`}><Button variant="primary">View Question</Button></Link> */}
                     </Card.Body>
                 </Card>
             </div>
@@ -46,7 +41,6 @@ class User extends Component {
 }
 
 function mapStateToProps ({  users }, { id }) {
-    // const authorId = questions[id].author;
     const authorName = users[id].name;
     const answeredLength = Object.keys(users[id].answers).length;
     const questionsLength = users[id].questions.length;
