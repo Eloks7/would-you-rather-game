52d6ff2c59e3b836a9535795ce7b204bd2f05834
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-20 11:37:01 +0100

    Update QuestionCard.js

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -10,7 +10,6 @@ class QuestionCard extends Component {
         return (
             <div style={{ display: 'inline' }}>
                 <Card style={{ width: '24rem', marginBottom: '32px', display: 'inline-block'}}>
-                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                     <Card.Body>
                         <Card.Title style={{ fontWeight: 500 }}>Would you rather</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted" style={{fontSize: '14px'}}>author {authorName}</Card.Subtitle>
