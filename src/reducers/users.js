8c0b7351e6abf72310cca699b01ac95cba4df0ac
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-14 21:13:04 +0100

    Immer implementation

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS, ADD_CREATED_QUESTION, SAVE_USER_ANSWER } from "../actions/users";
+import produce from "immer";
 
 export default function users (state = {}, action) {
     switch(action.type) {
@@ -9,13 +10,16 @@ export default function users (state = {}, action) {
             }
         case ADD_CREATED_QUESTION:
             const { id, authedUser } = action;
-            return {
-                ...state,
-                [action.authedUser]: {
-                    ...state[action.authedUser],
-                    questions: state[action.authedUser].questions.concat([id])
-                }
-            }
+            // return {
+            //     ...state,
+            //     [action.authedUser]: {
+            //         ...state[action.authedUser],
+            //         questions: state[action.authedUser].questions.concat([id])
+            //     }
+            // }
+            return produce(state, (draft) => {
+                draft[authedUser].questions.concat([id])
+            })
         case SAVE_USER_ANSWER:
             const { answer, qid } = action;
 
